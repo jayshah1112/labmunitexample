cbced4de9188b8bf8804839d261b056d090bab0e
akash-prajapati <akash.prajapati@gmail.com>
2017-10-06 15:52:25 +0530

    First commit

---
+++ src/main/java/com/mulesoft/training/Calc.java
@@ -0,0 +1,13 @@
+package com.mulesoft.training;
+
+public class Calc {
+	
+	public Integer square(Integer number){
+		return number*number;
+	}
+	
+	public Integer negate(Integer number){
+		return -number;
+	}
+
+}
